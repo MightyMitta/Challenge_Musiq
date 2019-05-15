f76860ccb54ef8da3e9a0f73e7e8e48fc4624256
Mitchel <mitcheltieges@hotmail.com>
2019-05-15 11:51:55 +0200

    Created Images folder and changed Source to Image path.

--- Musiq/Musiq/Model/Model.Designer.cs
+++ Musiq/Musiq/Model/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Mitchel\source\repos\MightyMitta\Challenge_Musiq\Musiq\Musiq\Model\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Mitchel\Source\Repos\MightyMitta\Challenge_Musiq\Musiq\Musiq\Model\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
